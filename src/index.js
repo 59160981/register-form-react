32438a49e5409877f2836adba31dfd25ccd141c0
Sarawut Sukpot <59160981@my.buu.ac.th>
2019-10-17 02:29:12 +0700

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+const Cryptr = require('cryptr')
+const cryptr = new Cryptr('myTotalySecretKey')
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class RegisterForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      user: {
+        firstname: "",
+        lastname: "",
+        username: "",
+        password: ""
+      }
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  handleChange(event, type) {
+    let user = this.state.user
+    if(type === "password"){
+        user[type] = cryptr.encrypt(event.target.value);
+    }else{
+        user[type] = event.target.value
+    }
+    this.setState({
+        user
+    })
+  }
+
+  handleSubmit(event) {
+    let data = this.state.user
+    console.log(JSON.stringify(data))
+    event.preventDefault()
+  }
+
+  render() {
+    return (
+      <form className='regisform' onSubmit={this.handleSubmit}>
+        <p>
+          <label>first name : </label>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={(e) => this.handleChange(e, 'firstname')}
+          />
+        </p>
+        <p>
+          <label>last name : </label>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={(e) => this.handleChange(e, 'lastname')}
+          />
+        </p>
+        <p>
+          <label>username : </label>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={(e) => this.handleChange(e, 'username')}
+          />
+        </p>
+        <p>
+          <label>password : </label>
+          <input
+            type="password"
+            value={this.state.value}
+            onChange={(e) => this.handleChange(e, 'password')}
+          />
+        </p>
+        <input type="submit" value="Submit" />
+      </form>
+    )
+  }
+}
+
+ReactDOM.render(<RegisterForm />, document.getElementById("root"))
